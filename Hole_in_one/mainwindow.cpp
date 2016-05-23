c6bbf24263bdca46059719edfc4ba19898e9b767
Victoria Hauzeneder <Victoria Hauzeneder>
2016-05-23 11:34:09 +0200

    Papierkneul.png

--- Hole_in_one/mainwindow.cpp
+++ Hole_in_one/mainwindow.cpp
@@ -1,14 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPainter>
+#include <QPixmap>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QLabel mylabel;
+    mylabel.setPixmap("Papierkneul.png");
+    mylabel.show();
+   // QPixmap pix(200,100);
+   // QPainter painter();
+   // painter.drawPixmap(20,30,10,10,&pix);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
---
+++ Hole_in_one/object.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+
+class Object : QWidget
+{
+public:
+    Object();
+};
+
+#endif // OBJECT_H
