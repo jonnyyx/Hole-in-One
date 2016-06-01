ef97ea2478f7ac7826fd6ce920e818f2ebeb083d
Jonas Vith <Jonas Vith>
2016-06-01 13:46:02 +0200

    delete

---
+++ Hole_in_one/levelmenu.cpp
@@ -0,0 +1,6 @@
+#include "levelmenu.h"
+
+Levelmenu::Levelmenu()
+{
+
+}
---
+++ Hole_in_one/levelmenu.h
@@ -0,0 +1,11 @@
+#ifndef LEVELMENU_H
+#define LEVELMENU_H
+
+
+class Levelmenu
+{
+public:
+    Levelmenu();
+};
+
+#endif // LEVELMENU_H
