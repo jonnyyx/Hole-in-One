11dbf662eaf1880a4c02d537b1e318fbd1d1988c
Victoria Hauzeneder <Victoria Hauzeneder>
2016-06-05 16:07:26 +0200

    master

---
+++ GUI only/button.cpp
@@ -0,0 +1,50 @@
+#include "button.h"
+#include <QGraphicsTextItem>
+#include <QBrush>
+
+Button::Button(QString buttonName, QGraphicsItem *parent) : QGraphicsRectItem(parent)
+{
+    /*Rectangle for buttons!*/
+    setRect(0,0,250,80);
+    QBrush buttonBrush;
+     /*!solid color*/
+    buttonBrush.setStyle(Qt::SolidPattern);
+    buttonBrush.setColor(Qt::darkRed);
+     /*!now set the brush*/
+    setBrush(buttonBrush);
+
+     /*!Text on the scene*/
+    text = new QGraphicsTextItem(buttonName, this);
+    int x = rect().width()/2 - text->boundingRect().width()/2;
+    int y = rect().height()/2 - text->boundingRect().height()/2;
+    text->setPos(x,y);
+
+     /*!Hovering mouse*/
+    setAcceptHoverEvents(true);
+
+}
+
+void Button::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    emit clicked();
+}
+
+void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
+{
+     /*!when hovering -> change Color*/
+    QBrush buttonBrush;
+    buttonBrush.setStyle(Qt::SolidPattern);
+    buttonBrush.setColor(Qt::red);
+    setBrush(buttonBrush);
+}
+
+void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
+{
+     /*!Change color back*/
+    QBrush buttonBrush;
+    buttonBrush.setStyle(Qt::SolidPattern);
+    buttonBrush.setColor(Qt::darkRed);
+    setBrush(buttonBrush);
+}
+
+
---
+++ GUI only/hole_in_one.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-01T12:54:46
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = hole_in_one
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        gui.cpp \
+    button.cpp
+
+HEADERS  += gui.h \
+    button.h \
+    main.h
+
+FORMS    +=
